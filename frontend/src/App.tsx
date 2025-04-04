17c83e00fcfd4987fc6ce19b0f9a83549da52da4
l-marcel <lmgh1312@gmail.com>
2025-04-04 12:35:02 -0300

    [style] Formatando código do frontend com prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} /> </Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />{" "}
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,59 +1,61 @@
 import { useState } from "react";
-import styles from './Login.module.scss';
+import styles from "./Login.module.scss";
 
 export default function Register() {
-    const [formData, setFormData] = useState({
-        email: "", senha: "", nome: "",
-    })
+  const [formData, setFormData] = useState({
+    email: "",
+    senha: "",
+    nome: "",
+  });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    if (response.ok) {
+      alert("Usuário registrado com sucesso!");
+    } else {
+      alert("Erro ao registrar usuário.");
     }
-
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        const response = await fetch("http://localhost:8080/users", {
-
-            method: "POST", headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData),
-
-        }); if (response.ok) {
-            alert("Usuário registrado com sucesso!");
-        } else {
-            alert("Erro ao registrar usuário.");
-        }
-    };
-    return (
-        <div className={styles.container}>
-            <h2 className={styles.title}>Registro</h2>
-            <form className={styles.form} onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="nome"
-                    placeholder="Nome"
-                    className={styles.input}
-                    onChange={handleChange}
-                />
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    className={styles.input}
-                    onChange={handleChange}
-                />
-                <input
-                    type="password"
-                    name="senha"
-                    placeholder="Senha"
-                    className={styles.input}
-                    onChange={handleChange}
-                />
-                <button type="submit" className={styles.button}>
-                    Registrar
-                </button>
-            </form>
-        </div>
-    );
-};
+  };
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Registro</h2>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="nome"
+          placeholder="Nome"
+          className={styles.input}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className={styles.input}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="senha"
+          placeholder="Senha"
+          className={styles.input}
+          onChange={handleChange}
+        />
+        <button type="submit" className={styles.button}>
+          Registrar
+        </button>
+      </form>
+    </div>
+  );
+}
