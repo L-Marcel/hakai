2aa2a896cc38c941f132fc706cc32c5d51c716c9
l-marcel <lmgh1312@gmail.com>
2025-04-26 17:41:43 -0300

    [feat] Criando primeira sala estaticamente

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -4,6 +4,24 @@ import { EditIcon } from "@components/Icons/EditIcon";
 import { EraserIcon } from "@components/Icons/EraserIcon";
 
 export default function Card() {
+  const createRoom = async() => {
+    const response = await fetch("http://localhost:8080/rooms/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "uuid": "4ad9e19e-8f9b-4b93-a7a1-17ea469bd455"
+      })
+    });
+
+    if(response.ok) {
+      console.log(await response.json());
+    } else {
+      console.log(response.status);
+    };
+  };
+
   return (
     <li className={styles.card}>
       <div className={styles.content}>
@@ -17,7 +35,7 @@ export default function Card() {
         <button disabled>
           <EditIcon />
         </button>
-        <button>
+        <button onClick={createRoom}>
           <PlayIcon />
           Iniciar
         </button>
