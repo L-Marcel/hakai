6e39d9991cd54186d0040c1d60c103d650578be2
l-marcel <lmgh1312@gmail.com>
2025-05-03 11:28:59 -0300

    [feat] Adicionando meio de juntar a uma sala mesmo logado

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,9 +1,10 @@
 import AuthGuard from "@components/Guards/AuthGuard";
 import styles from "./index.module.scss";
 import Card from "@components/Card";
-import { FaSignOutAlt } from "react-icons/fa";
+import { FaPlus, FaSignOutAlt } from "react-icons/fa";
 import useAuth from "../../stores/useAuth";
 import Button from "@components/Button";
+import CheckRoomForm from "@components/Forms/CheckRoomForm";
 
 export default function DashboardPage() {
   return (
@@ -20,7 +21,13 @@ function Page() {
   return (
     <main className={styles.main}>
       <header className={styles.header}>
-        <Button onClick={logout}>
+        <div>
+          <Button theme="full-orange" disabled>
+            <FaPlus />
+          </Button>
+          <CheckRoomForm />
+        </div>
+        <Button theme="full-orange" onClick={logout}>
           <FaSignOutAlt />
         </Button>
       </header>
