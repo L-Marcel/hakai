024c739b19326b5400143d2f038a4094176299dc
l-marcel <lmgh1312@gmail.com>
2025-05-13 08:51:28 -0300

    [chore] Removendo comentários

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -19,9 +19,8 @@ export default function Card({ game }: CardProps) {
   return (
     <li className={styles.card}>
       <div className={styles.content}>
-        <h1>{game.title}</h1> {/* Exibindo o título do jogo */}
-        <p>{game.questions.length} perguntas contextualizadas</p>{" "}
-        {/* Exibindo a quantidade de perguntas */}
+        <h1>{game.title}</h1>
+        <p>{game.questions.length} perguntas contextualizadas</p>
       </div>
       <div className={styles.buttons}>
         <Button rounded="full" disabled>
