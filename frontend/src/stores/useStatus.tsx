40cb9ec3ff6d0e166f9821b41fa46d76d55ebe54
Maria Clara Fernandes <fernandes.mcfo@gmail.com>
2025-05-26 18:55:32 -0300

    [feat] Implementando feedback de IA

---
+++ frontend/src/stores/useStatus.tsx
@@ -0,0 +1,13 @@
+import {create} from "zustand";
+
+type StatusState = {
+    generationStatus?: string;
+    setGenerationStatus: (status?: string) => void;
+};
+
+const useGenerationStatus = create<StatusState>((set) => ({
+    generationStatus: undefined,
+    setGenerationStatus: (status) => set({ generationStatus: status }),
+}));
+
+export default useGenerationStatus;
