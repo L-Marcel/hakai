11ec8a9a5c6f07826ada7c63906b528b4a0ec649
l-marcel <lmgh1312@gmail.com>
2025-06-29 19:01:54 -0300

    [chore] Atualizando framework, modelo e migrando implementação da estratégia de distribuição

---
+++ backend/src/main/java/app/hakai/backend/strategies/VariantsDistributionByDifficulty.java
@@ -0,0 +1,33 @@
+package app.hakai.backend.strategies;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.kahai.framework.services.strategies.VariantsDistributionStrategy;
+import org.kahai.framework.transients.Participant;
+import org.kahai.framework.transients.QuestionVariant;
+
+public class VariantsDistributionByDifficulty implements VariantsDistributionStrategy {
+    @Override
+    public Optional<QuestionVariant> selectVariant(
+        Participant participant, 
+        List<QuestionVariant> variants
+    ) {
+        return variants.stream()
+            .filter(
+                (variant) -> variant.getDifficulty() == participant.getCurrentDifficulty()
+            ).findAny();
+    };
+    
+    @Override
+    public List<QuestionVariant> selectVariants(
+        Participant participant, 
+        List<QuestionVariant> variants
+    ) {
+        return variants.stream()
+            .filter(
+                (variant) -> variant.getDifficulty() == participant.getCurrentDifficulty()
+            ).collect(Collectors.toList());
+    };
+};;
